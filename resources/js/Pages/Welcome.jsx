272b7b921aecc6f1ca67104d56faaca7aee14916
Massinissa648 <massi27000@hotmail.fr>
2025-03-26 09:47:24 +0100

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -37,7 +37,16 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     />
                                 </svg>
                             </div>
-                            <nav className="-mx-3 flex flex-1 justify-end">
+                            <nav className="-mx-3 flex flex-1 justify-end items-center">
+                                 <button 
+        onClick={() => setIsMenuOpen(true)}
+        className="p-2 text-gray-500 hover:text-gray-700 dark:hover:text-white"
+    >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01" />
+        </svg>
+    </button>
+                                
                                 {auth.user ? (
                                     <Link
                                         href={route('dashboard')}
