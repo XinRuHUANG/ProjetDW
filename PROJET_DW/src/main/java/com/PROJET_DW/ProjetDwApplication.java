8345ff2cc6f522a4fed5e22bf8c1e3f92ea59ad0
XR <huangxre@gmail.com>
2025-03-26 20:37:07 +0100

    Avec Spring

---
+++ PROJET_DW/src/main/java/com/PROJET_DW/ProjetDwApplication.java
@@ -0,0 +1,13 @@
+package com.PROJET_DW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDwApplication.class, args);
+	}
+
+}
