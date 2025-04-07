58b0074af948abcf3bcbd566d865e742d20a21dd
Massinissa648 <massi27000@hotmail.fr>
2025-04-08 00:08:45 +0200

    Update Index.jsx

--- ProjetDW/resources/js/Pages/Members/Index.jsx
+++ ProjetDW/resources/js/Pages/Members/Index.jsx
@@ -1,1 +1,27 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Link, Head } from '@inertiajs/react';
+
+export default function MembersIndex({ members }) {
+    return (
+        <AuthenticatedLayout>
+            <Head title="Liste des Membres" />
+            <div className="p-6">
+                <h1 className="text-2xl font-bold mb-6">Liste des Membres</h1>
+
+                <ul className="space-y-4">
+                    {members.map((member) => (
+                        <li key={member.idUser} className="border-b pb-2">
+                            <Link
+                                href={route('members.show', member.idUser)}
+                                className="text-blue-600 hover:underline"
+                            >
+                                {member.firstName} {member.lastName}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
 
