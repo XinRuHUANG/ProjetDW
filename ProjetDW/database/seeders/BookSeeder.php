71ce093954f7375b7f127d60593b743f64e78b6d
Massinissa648 <massi27000@hotmail.fr>
2025-04-08 00:18:02 +0200

    Update BookSeeder.php

--- ProjetDW/database/seeders/BookSeeder.php
+++ ProjetDW/database/seeders/BookSeeder.php
@@ -1,1 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class BookSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('books')->insert([
+            [
+                'title' => 'Le Seigneur des Anneaux',
+                'author' => 'J.R.R. Tolkien',
+                'yearPublished' => 1954,
+                'category' => 'fantasy',
+                'summary' => 'Une aventure épique pour détruire l\'anneau unique.',
+                'status' => 'Available',
+                'stock' => 3,
+                'cover' => 'https://m.media-amazon.com/images/I/71jLBXtWJWL._AC_UF1000,1000_QL80_.jpg',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'title' => 'Harry Potter à l\'école des sorciers',
+                'author' => 'J.K. Rowling',
+                'yearPublished' => 1997,
+                'category' => 'fantasy',
+                'summary' => 'Les débuts de Harry à Poudlard.',
+                'status' => 'Available',
+                'stock' => 5,
+                'cover' => 'https://m.media-amazon.com/images/I/71-++hbbERL._AC_UF1000,1000_QL80_.jpg',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
 
