17bafd91ee278be17ce220c7f5cd4be2c46965f2
Massinissa648 <massi27000@hotmail.fr>
2025-04-08 00:13:15 +0200

    Update MemberController.php

--- ProjetDW/app/Http/Controllers/MemberController.php
+++ ProjetDW/app/Http/Controllers/MemberController.php
@@ -1,1 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class MemberController extends Controller
+{
+    public function index()
+    {
+        $members = User::where('userType', '!=', 'Admin')->get(); // ou '3' si c'est codé comme ça
+
+        return Inertia::render('Members/Index', [
+            'members' => $members,
+        ]);
+    }
+
+    public function show(User $user)
+    {
+        return Inertia::render('Members/Show', [
+            'member' => $user,
+        ]);
+    }
+}
 
