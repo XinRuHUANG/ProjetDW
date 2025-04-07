dcd1a0e6c6c3b358f3d440ef577908381d9e5274
Massinissa648 <massi27000@hotmail.fr>
2025-04-08 00:20:10 +0200

    Update UserFactory.php

--- ProjetDW/database/factories/UserFactory.php
+++ ProjetDW/database/factories/UserFactory.php
@@ -3,42 +3,27 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'firstName' => $this->faker->firstName(),
+            'lastName' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'), // mot de passe par défaut
+            'userType' => 'Member1',
+            'age' => $this->faker->numberBetween(18, 65),
+            'birthDate' => $this->faker->date('Y-m-d'),
+            'gender' => $this->faker->randomElement(['Male', 'Female']),
+            'points' => $this->faker->numberBetween(0, 1000),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
