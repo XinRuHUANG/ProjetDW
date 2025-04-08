79fdeafce372cd272b68dd4990ece98669843b16
Massinissa648 <massi27000@hotmail.fr>
2025-04-08 16:49:06 +0200

    Create ConnectedDeviceController.php

---
+++ ProjetDW/app/Http/Controllers/ConnectedDeviceController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ConnectedDeviceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Equipment::query();
+
+        if ($request->has('type') && $request->type !== '') {
+            $query->where('equipmentType', $request->type);
+        }
+
+        if ($request->has('status') && $request->status !== '') {
+            $query->where('status', $request->status);
+        }
+
+        $devices = $query->get();
+
+        return Inertia::render('ConnectedDevices/Index', [
+            'devices' => $devices,
+            'filters' => $request->only('type', 'status')
+        ]);
+    }
+}
